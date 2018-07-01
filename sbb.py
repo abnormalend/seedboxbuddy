bc7f2f4eab88d3ee2c258a393e9055ef0648020f
Brent Rogers <brentarogers@gmail.com>
2018-07-01 12:59:18 -0400

    adding check for / on download path

--- sbb.py
+++ sbb.py
@@ -104,6 +104,8 @@ config = getSettings()
 limit_hours = str2bool(config['settings']['limit_hours'])
 start_time = config['settings']['start_time'].split(':')
 stop_time = config['settings']['stop_time'].split(':')
+if config['settings']['localSavePath'][-1:] is not '/':
+    config['settings']['localSavePath'] = config['settings']['localSavePath'] + '/'
 # Now the main program loop.  If we have support for other servers we can add
 #   that here and load something besides rutorrent
 
