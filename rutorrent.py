0b9ba535108c74d9a653a8a4ae6dd907245d7654
Brent Rogers <brentarogers@gmail.com>
2025-01-30 09:32:22 -0500

    fix the right error

--- rutorrent.py
+++ rutorrent.py
@@ -90,7 +90,7 @@ class RuTorrent:
                 json_data = response.json()
             except Exception as e:
                 self.logger.error("something has gone wrong, unable to download torrent lists.  Will retry in " + str(self.grabtorrent_retry_delay) + " seconds.")
-                # self.logger.error(e)
+                self.logger.error(e)
                 time.sleep(self.grabtorrent_retry_delay)
             else:
                 break
